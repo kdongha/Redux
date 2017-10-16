8e5e8a6455b2623222b7bee87b5992641c254759
kdongha <kdongha1597@gmail.com>
2017-10-16 19:45:42 +0900

    done

---
+++ src/components/Value.js
@@ -0,0 +1,22 @@
+import React, { Component} from 'react';
+import PropTypes from 'prop-types';
+const propTypes = {
+  number: PropTypes.number
+};
+const defaultProps = {
+  number : -1
+};
+class Value extends Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return(
+            <div>
+              <h1>{this.props.number}</h1>
+            </div>
+        );
+    }
+}
+Value.defaultProps = defaultProps;
+export default Value;
